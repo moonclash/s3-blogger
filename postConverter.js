b5c75eb7f00743f07b6ddc45d39fab45457777b6
moonclash <emladenoff@gmail.com>
2022-11-28 12:53:56 +0100

    add index skeleton and link building

--- postConverter.js
+++ postConverter.js
@@ -24,3 +24,20 @@ sourcePaths.forEach(path => {
         })
     });
 });
+
+
+fs.readdir('dist/posts/', (error, data) => {
+    if (error) {
+        return;
+    }
+    convertedPosts = data.map(post => `<a href="dist/posts/${post}">${post}</a>`);
+    fs.readFile('index-skeleton.html', 'utf-8', (error, data) => {
+        if (error) {
+            throw new Error();
+        }
+        fs.writeFile('dist/index.html', data.replace(/<!-- articles go here -->/gi, convertedPosts.join('\n')), 'utf-8',
+         (err, data) => {
+
+        })
+    })
+})
